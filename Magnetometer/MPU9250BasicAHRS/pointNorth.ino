24f11540a0f46b0348384e8bc46fae11f905b480
Eric Leask <16efl@queensu.ca>
2019-10-04 20:07:52 -0400

    Create pointNorth.ino

---
+++ Magnetometer/MPU9250BasicAHRS/pointNorth.ino
@@ -0,0 +1,9 @@
+//Purpose of code is simple, point car northwards
+
+//Steps 
+
+//read magnetometer input
+//what way we are currently pointing
+//determine which direction to turn to face north
+//rotate towards north
+//loop
