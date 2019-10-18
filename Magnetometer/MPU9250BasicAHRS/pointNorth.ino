eb80b28bc78c335d6ae0bdfbdb2f754126d99ae7
Eric Leask <16efl@queensu.ca>
2019-10-18 15:25:10 -0400

    Troubleshoot notes, header file
    Add troubleshooting notes, split code into header file

--- Magnetometer/MPU9250BasicAHRS/pointNorth.ino
+++ Magnetometer/MPU9250BasicAHRS/pointNorth.ino
@@ -1,9 +1,44 @@
 //Purpose of code is simple, point car northwards
-
+//#include "MPU9250BasicAHRS"
+#include "MPU9250BasicAHRS.h"
+#include <math.h>
 //Steps 
+//Set up
+float magVal[3] = {0, 0, 0};
+int motorFPin = 9;
+int motorBPin = 10;
 
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(motorFPin, OUTPUT);
+ pinMode(motorBPin, OUTPUT);
+ magSetup();
+}
+
+void loop() {
 //read magnetometer input
+getMagVal(magVal);
 //what way we are currently pointing
+float radDirection = atan2(magVal[1], magVal[0]);
+
+float degDirection = (radDirection + M_PI) * (360.0 / (2 * M_PI));
+
+if (radDirection<0){
+	radDirection=fabs(radDirection);
+	//As it gets closer to the correct direction it slows down
+	int speed = (radDirection/M_PI)*1023;
+  analogWrite(motorFPin, speed);
+  analogWrite(motorBPin, 0);
+	
+}else{
+	radDirection=fabs(radDirection);
+	//As it gets closer to the correct direction it slows down
+	int speed = (radDirection/M_PI)*1024;
+  analogWrite(motorBPin, speed);
+  analogWrite(motorFPin, 0);
+}
+}
+
 //determine which direction to turn to face north
 //rotate towards north
 //loop
